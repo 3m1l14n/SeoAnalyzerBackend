874c5ea2a4cc91fd58705d4c021fe6ad5167e6ae
3m1l14n <lokus3@gmail.com>
2022-01-31 21:41:57 +0000

    Changes to make the code more OOP (ㆆ_ㆆ)

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -15,20 +15,16 @@ class TestController
 	/**
 	 * @Route("/analyze")
 	 */
-	public function homepage(Request $request)
+	public function RequestUrl(Request $request)
 	{
 		$url = $request->query->get('url');
 
 		$links = new LinksAgregator();
-		$linksAssoc = $links->getLinks($url);
-		$linksJoined = Array();
-		foreach ($linksAssoc as $key => $value){
-			$linksJoined[] = "$key - <b>$value</b>";
-		}
-
-		$show = implode("<BR>", $linksJoined);
-
-		return new Response("$show");
+		$links = $links->getLinkProperties($url); //Returns array of Link objects
+        
+		$json = json_encode($links, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+		Helper::print_r2($json);
+		return new Response($json);
 	}
 
 }
@@ -46,7 +42,7 @@ class LinksAgregator{
 		$header[] = "Keep-Alive: 300";
 		$header[] = "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7";
 		$header[] = "Accept-Language: en-us,en;q=0.5";
-		$header[] = "Pragma: "; // browsers keep this blank.
+		$header[] = "Pragma: ";
 
 		curl_setopt($curl, CURLOPT_URL, $this->url);
 		curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0');
@@ -61,30 +57,59 @@ class LinksAgregator{
 		return $html;
 	}
 
-    public function getLinks(String $url): array{
+    public function getLinkProperties(String $url){
 		$this->url = $url;
-
+		$linkObjects = array();
         $dom = new DOMDocument();
 		//$dom->strictErrorChecking = false; //Ta linia nie dziala. Musialem uzyc linie nizej wyciszania bledow znakiem @
         @$dom->loadHTML($this->getRemoteFileContent()); //Without @ ErrorException: DOMDocument::loadHTML(): Tag header invalid in Entity
-        $DOMNodeList = $dom->getElementsByTagName("a");
-		
-        foreach ($DOMNodeList as $link) {
-			$theLink = $this->url.$link->getAttribute("href");
+        
+		//get anchors
+		$anchor = $dom->getElementsByTagName("a");
+		foreach ($anchor as $link) {
+			$theLink = $link->getAttribute("href");
 
-			if (strpos("http", $theLink)){
+			//If the link is relative
+			if (!strpos($theLink, "ttp")){
 				$eachLink = $this->url.$theLink;
 			}
 			else {
                 $eachLink = $theLink;
             }
-			
-            $eachAnchor = $link->textContent;
-            $this->links[$eachLink] = $eachAnchor;
-        }
 
-        return $this->links;
+			//IF the link contains body. @NULL = "NULL"
+			$eachLinkBody = $link->textContent ? $eachLinkBody = $link->textContent : "NULL";
+
+             $this->links[$eachLink] = $eachLinkBody;		
+
+
+				//get title
+				$title =  $dom->getElementsByTagName("title");
+				foreach ($title as $value) {
+				}
+		
+
+            $linkObjects[] = new Link($eachLink, $eachLinkBody);
+        }
+		return $linkObjects;
     }
+
+}
+
+class Link{
+	public $url = "";
+	public $urlContent = "";
+	public $urlDecription = "";
+	public $pageTitle = "";
+	public $relAttrb = "";
+	public $nofallow = FALSE;
+	public $dofallow = FALSE;
+
+	public function __construct($url, $urlContent){
+		$this->url = $url;
+		$this->urlContent = $urlContent;
+	}
+	
 
 }
 
