89452be7e4b07a91824a70b9beff8ee7edb03548
3m1l14n <lokus3@gmail.com>
2022-02-02 22:01:54 +0000

    Met problem with run out of memory. MR Krzysztof please kick in :)

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -17,25 +17,85 @@ class TestController
 	 */
 	public function RequestUrl(Request $request)
 	{
-		$url = $request->query->get('url');
+		$webpage = new WebPage($request->query->get('pageaddress'));
+		$webpage->crawler->run();
 
-		$links = new LinksAgregator();
-		$links = $links->getLinkProperties($url); //Returns array of Link objects
-        
-		$json = json_encode($links, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-		Helper::print_r2($json);
-		return new Response($json);
+		//Helper::print_r2($webpage);
+		// Helper::print_r2($webpage->pageObjects);
+		Helper::print_r2($webpage->crawler->run());
+		//Helper::print_r2($webpage->pageObjects[0]->linkObjects);
+		//$webpage->getPage('contact')->getLinkCount(); //Returns number of links of specific page. 
+
+
+		//$json = json_encode($links, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+		return new Response("");
 	}
 
 }
-
-class LinksAgregator{
-    private $links = array();
+   
+class CrawlerService
+{
+	private $dom = "";
 	private $url = "";
+	private $depth;
 
-	private function getRemoteFileContent(): string{
+	public function __construct($url, $depth = 5)
+	{
+		$this->url = $url;
+		$this->depth =  $depth;
+		$dom = new DOMDocument();
+		@$dom->loadHTML($this->getContent($this->url));  //Return DOMDocument object
+		$this->dom = $dom;
+	}
+    
+    public function run($url = "WebPageInit", $depth = 5)
+	{
+		$url = "WebPageInit" ? $url = $this->url : $url = $url;
+
+		$seen = array();
+
+		if (isset($seen[$url]) || $depth === 0)
+		{
+			return;
+		}
+
+		$anchor = $this->dom->getElementsByTagName("a");
+		foreach ($anchor as $link)
+		{
+			$theLink = $link->getAttribute("href");
+
+			//If the link is relative
+			if (!strpos($theLink, "ttp"))
+			{
+				$href = $this->url.$theLink;
+			}
+			else 
+			{
+                $href = $theLink;
+            }
+			//IF the link contains body. @NULL = "NULL"
+			$anchorText = $link->nodeValue ? $anchorText = $link->nodeValue : "NULL";
+
+            $linkObjects[$href] = new Link($href, $anchorText);
+
+			$this->run($href, $depth = -1);
+        }
+		//Return array of linkObjects
+		return $linkObjects;
+    }
+
+	public function getTitle()
+	{
+				//get title
+				$title =  $this->dom->getElementsByTagName("title")->item(0)->nodeValue;
+				return $title;		
+	}
+
+	private function getContent($url): string{
 		
 		$curl = curl_init();
+
+		//CURL headers
 		$header[0] = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
 		$header[] = "Cache-Control: max-age=0";
 		$header[] = "Connection: keep-alive";
@@ -44,7 +104,8 @@ class LinksAgregator{
 		$header[] = "Accept-Language: en-us,en;q=0.5";
 		$header[] = "Pragma: ";
 
-		curl_setopt($curl, CURLOPT_URL, $this->url);
+		//CURL options
+		curl_setopt($curl, CURLOPT_URL, $url);
 		curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0');
 		curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
 		curl_setopt($curl, CURLOPT_ENCODING, 'gzip,deflate');
@@ -57,65 +118,90 @@ class LinksAgregator{
 		return $html;
 	}
 
-    public function getLinkProperties(String $url){
-		$this->url = $url;
-		$linkObjects = array();
-        $dom = new DOMDocument();
-		//$dom->strictErrorChecking = false; //Ta linia nie dziala. Musialem uzyc linie nizej wyciszania bledow znakiem @
-        @$dom->loadHTML($this->getRemoteFileContent()); //Without @ ErrorException: DOMDocument::loadHTML(): Tag header invalid in Entity
-        
-		//get anchors
-		$anchor = $dom->getElementsByTagName("a");
-		foreach ($anchor as $link) {
-			$theLink = $link->getAttribute("href");
-
-			//If the link is relative
-			if (!strpos($theLink, "ttp")){
-				$eachLink = $this->url.$theLink;
-			}
-			else {
-                $eachLink = $theLink;
-            }
-
-			//IF the link contains body. @NULL = "NULL"
-			$eachLinkBody = $link->textContent ? $eachLinkBody = $link->textContent : "NULL";
-
-             $this->links[$eachLink] = $eachLinkBody;		
-
-
-				//get title
-				$title =  $dom->getElementsByTagName("title");
-				foreach ($title as $value) {
-				}
-		
-
-            $linkObjects[] = new Link($eachLink, $eachLinkBody);
-        }
-		return $linkObjects;
-    }
-
 }
 
-class Link{
-	public $url = "";
-	public $urlContent = "";
-	public $urlDecription = "";
-	public $pageTitle = "";
-	public $relAttrb = "";
-	public $nofallow = FALSE;
-	public $dofallow = FALSE;
+class WebPage
+{
+	public static $url = "";
+	private $name = "";
+	public $pageObjects = array(); //Tablica wszystkich podstron. Docelowo private
+	public $crawler;
 
-	public function __construct($url, $urlContent){
-		$this->url = $url;
-		$this->urlContent = $urlContent;
+	public function __construct(string $pageAddress)
+	{
+		self::$url = $pageAddress;
+		$this->crawler = new CrawlerService(self::$url, 5);
+		//Helper::print_r2($webPage->getLinks()[4]->getUrl());
 	}
-	
+
+	public function addPage($page)
+	{
+		$this->pageObjects = $page;
+	}
+
+	public function getUrl()
+	{
+		return $this->url;
+	}
+
+	public function getPage(string $page)
+	{
+		return $this->pageObjects[$page];
+	}
+}
+
+class Page extends WebPage
+{
+	private $title = "";
+	private $description = "";
+	private $linkObjects = array(); //Tablica obiektow wszystkich linkow na podstronie
+
+	public function __construct()
+	{
+		$this->linkObjects[] = new Link();
+	}
+
+	public function getLinkCount()
+	{
+		count($this->linkObjects);
+	}
+}
+
+class Link extends Page
+{
+	private $anchorText = "";
+	private $linkUrl = "";
+	private $rel = "";
+
+	public function __construct($url, $anchorText)
+	{
+		$this->linkUrl = $url;
+		$this->anchorText = $anchorText;
+	}
+
+	public function getUrl()
+	{
+		return $this->linkUrl;
+	}
+
+	public function getAnchor()
+	{
+
+	}
 
 }
+
+class JsonConverter
+{
+
+}
+
+
+
 
 abstract class Helper {
 	static public function print_r2($var){
-		echo "<pre>";
+		echo "<pre><HR><BR>";
 		print_r($var);
 		echo "</pre>";
 	}
